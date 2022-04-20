d157772a7254b7840dfe90bd6629d6a964661c2b
jero98772 <jero98772@protonmail.com>
2022-04-20 16:12:16 -0500

    update

--- proyecto/codigo/alOtroLado/core/maps.py
+++ proyecto/codigo/alOtroLado/core/maps.py
@@ -4,3 +4,6 @@ class maps():
 	@app.route("/map20220418094140147118.html")
 	def map20220418094140147118():
 		return render_template("maps/map20220418094140147118.html")
+	@app.route("/map20220420124049508619.html")
+	def map20220420124049508619():
+		return render_template("maps/map20220420124049508619.html")
