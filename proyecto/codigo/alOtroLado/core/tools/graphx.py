ffe201ed3a26393a139c2833eb9e4850ca2a262f
jero98772 <jero98772@protonmail.com>
2022-04-22 14:30:28 -0500

    add conpol

--- proyecto/codigo/alOtroLado/core/tools/graphx.py
+++ proyecto/codigo/alOtroLado/core/tools/graphx.py
@@ -5,16 +5,19 @@ class node():
         self.weight=weight
         self.next=None
 
-class Graph:
+class graph:
     def __init__(self):
         self.graph={}            
 
     def addEdges(self,source,dest, weight): 
         vert=node(dest, weight)
         if source in self.graph:
-            self.graph[source]=self.graph[source].append(vert)
             vert.next=self.graph[source]
+            self.graph[source]=self.graph[source].append(vert)
         else:
             self.graph[source]=[vert]
         vertice.next=self.graph[source]
         self.graph[source]= vertice
+
+class dijkstra:
+    def __init__(self)
